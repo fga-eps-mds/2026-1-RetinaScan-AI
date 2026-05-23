024b63d1acc8d81e1878be97b30c8b08f9a826b9
cwtshh <gucosta1719@gmail.com>
2026-05-23 20:49:14 -0300

    feat: permite todas as origens

--- src/main.py
+++ src/main.py
@@ -50,7 +50,7 @@ def create_app() -> FastAPI:
 
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=settings.cors_origins,
+        allow_origins=["*"],
         allow_credentials=True,
         allow_methods=["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
         allow_headers=["*"],
